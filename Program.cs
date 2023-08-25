1c4af17710da7cb9b8a894b36fe0cc134ca8289f
juescoryisus <125850604+juescoryisus@users.noreply.github.com>
2023-08-24 22:55:04 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectores_y_tiempo_de_ejecucion
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int size;
+
+                Console.Write("Ingrese el tamaño del vector: ");
+                size = int.Parse(Console.ReadLine());
+
+                // Creación de un vector en tiempo de ejecución
+                int[] vector = new int[size];
+
+                // Inicialización del vector
+                for (int i = 0; i < size; i++)
+                {
+                    vector[i] = i * 2;
+                }
+
+                // Imprimir el contenido del vector
+                Console.WriteLine("Contenido del vector:");
+                foreach (int num in vector)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+
